deda29c0fb9784411e5eb5a5f1dffc8c65874c52
Amanda Beiner <aebeiner@gmail.com>
2020-03-28 23:18:57 -0400

    initial commit
    sign in working

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Homemade Apple', 'sans-serif'],
+      body: ['Lato', 'serif']
+    },
+    colors: {
+      teal: '#4ABDAC',
+      orange: "#FC4A1A"
+    },
+    screens: {
+      'phone': '640px',
+      'tablet': '1024px',
+      'desktop': '1280px',
+    }
+  }
+}
